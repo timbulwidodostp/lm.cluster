546511093b78812f34e89930e9918e875741be5b
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-06-17 09:44:55 +0700

    Use lm.cluster With (In) R Software

---
+++ lm.cluster/lm.cluster.R
@@ -0,0 +1,19 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Cluster Robust Standard Errors for Linear Models and General Linear Models Use lm.cluster With (In) R Software
+install.packages("miceadds")
+install.packages("sandwich")
+library("miceadds")
+library("sandwich")
+lm.cluster = read.csv("https://raw.githubusercontent.com/timbulwidodostp/lm.cluster/main/lm.cluster/lm.cluster.csv",sep = ";")
+# Estimation Cluster Robust Standard Errors for Linear Models and General Linear Models Use lm.cluster With (In) R Software
+lm.cluster <- miceadds::lm.cluster( data=lm.cluster, formula=read ~ hisei + female, cluster="idschool" )
+coef(lm.cluster)
+vcov(lm.cluster)
+summary(lm.cluster)
+# Cluster Robust Standard Errors for Linear Models and General Linear Models Use lm.cluster With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
