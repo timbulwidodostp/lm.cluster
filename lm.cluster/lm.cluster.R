ac6750b8c512c498a2f32bc4988d438ce0f0c453
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-06-23 22:35:33 +0700

    Use lm.cluster With (In) R Software

--- lm.cluster/lm.cluster.R
+++ lm.cluster/lm.cluster.R
@@ -4,8 +4,10 @@
 # Cluster Robust Standard Errors for Linear Models and General Linear Models Use lm.cluster With (In) R Software
 install.packages("miceadds")
 install.packages("sandwich")
+install.packages("pillar")
 library("miceadds")
 library("sandwich")
+library("pillar")
 lm.cluster = read.csv("https://raw.githubusercontent.com/timbulwidodostp/lm.cluster/main/lm.cluster/lm.cluster.csv",sep = ";")
 # Estimation Cluster Robust Standard Errors for Linear Models and General Linear Models Use lm.cluster With (In) R Software
 lm.cluster <- miceadds::lm.cluster( data=lm.cluster, formula=read ~ hisei + female, cluster="idschool" )
